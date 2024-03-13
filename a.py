cefbe562983e12921075173cf53805cd3c2dc0bb
Shuno <ducthinh1280@gmail.com>
2024-03-13 22:55:57 +0700

    new

--- a.py
+++ a.py
@@ -1,4 +1,10 @@
-print ('hello')
-print ("000a=2")
-print("asdssdf;fsd")
-print('a')
+def print_ds(ds):
+    for n in ds:
+        print(n)
+
+# Tạo một danh sách bất kỳ
+ds = [1, 2, 3, 4, 5]
+
+# In ra từng phần tử trong danh sách
+print("Các phần tử trong danh sách:")
+print_ds(ds)
